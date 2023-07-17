4bcbc3e0191b0701dac76b89847f525bcb01a12b
Ghislaine Aybram <ghislaine.aybram@gmail.com>
2023-07-17 16:40:38 +0200

    js ajouté

---
+++ script.js
@@ -0,0 +1,13 @@
+let button = document.getElementById("button")
+let nom= document.getElementById("text")
+
+const quelEstTonAge = async () => {
+let age = `https://api.agify.io/?name=${nom}`
+let data = await fetch(age);
+console.log(data);
+let response = await data.json();
+console.log(response)
+document.querySelector("p").innerHTML = `Vous avez ${response.age} ans`
+}
+
+button.addEventListener("click", quelEstTonAge)

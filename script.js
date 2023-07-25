98837aa67cfdda9d4a465dfe026ac3b0915e23f8
Ghislaine Aybram <ghislaine.aybram@gmail.com>
2023-07-25 10:51:03 +0200

    reglage animation fumee

--- script.js
+++ script.js
@@ -4,50 +4,51 @@ let reponseCorrecte = document.getElementById('button-reponse-correcte')
 let reponseIncorrecte = document.getElementById('button-reponse-incorrecte')
 let indicePays = 0;
 let countryId = '';
+let videoBoule = document.getElementById("animation");
+
+// lancement animation fumée dans boule de cristal
+const lancementAnimation = () => {
+    videoBoule.play().then(() => {
+        document.getElementById("statusText").innerHTML = "Yay ! La vidéo est lancée !";
+    }).catch((error) => {
+        document.getElementById("statusText").innerHTML = "Erreur: " + error;
+    })
+}
 
 button.addEventListener('click', () => {
     masquerDiv()
-    debutVideo()
+    lancementAnimation()
     guessMyNationality()
 })
 
 reponseCorrecte.addEventListener('click', () => {
-       guessMyAge()
+    guessMyAge()
 })
 
 reponseIncorrecte.addEventListener('click', () => {
-       indicePays += 1;
-       debutVideo();
-       guessMyNationality()
-}) 
+    indicePays += 1;
+    lancementAnimation();
+    guessMyNationality()
+})
 
-function masquerDiv()
-{
-  if (document.getElementById('reponse-utilisateur').style.display == 'none') {
-       document.getElementById('reponse-utilisateur').style.display = 'block';
-  }
-  else {
-       document.getElementById('reponse-utilisateur').style.display = 'none';
-  }
-}
-function debutVideo()
-{
-  if (document.getElementById('animation').style.display == 'none') {
-       document.getElementById('animation').style.display = 'block';
-  }
-  else {
-       document.getElementById('animation').style.display = 'none';
-  }
+// fonction pour faire apparaître les 2 boutons de réponse utilisateur
+function masquerDiv() {
+    if (document.getElementById('reponse-utilisateur').style.display == 'none') {
+        document.getElementById('reponse-utilisateur').style.display = 'block';
+    }
+    else {
+        document.getElementById('reponse-utilisateur').style.display = 'none';
+    }
 }
 
 async function guessMyAge() {
     let response = await fetch(`https://api.agify.io?name=${nom.value}&country_id=${countryId}`);
-   // let response = await fetch(`https://api.agify.io?name=${nom.value}`);
+    // let response = await fetch(`https://api.agify.io?name=${nom.value}`);
     let dataAge = await response.json()
     displayAge(dataAge)
 }
 
-function displayAge(dataAge){
+function displayAge(dataAge) {
     document.querySelector("#age").innerHTML = "Je devine que tu as " + dataAge.age + " ans"
 }
 
@@ -59,15 +60,10 @@ async function guessMyNationality() {
     displayNationality(dataNationality, dataCountry)
 }
 
-function displayNationality(dataNationality, dataCountry){
+function displayNationality(dataNationality, dataCountry) {
     const codePays = dataNationality.country[indicePays].country_id;
     countryId = codePays;
     document.querySelector("#nationality").innerHTML = "Je devine que ton pays est : " + dataCountry.result.result[codePays];
     const img = document.querySelector("#flag");
     img.src = `https://flagsapi.com/${codePays}/shiny/64.png`;
 }
-
-
-
-
-
